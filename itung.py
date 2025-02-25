ec9e53a670d1d9eda4cece148adbcb2a89f6ab29
Denji Wak <denjicode@gmail.com>
2025-02-25 22:29:25 +0700

    Mengontol

---
+++ itung.py
@@ -0,0 +1,6 @@
+# Program untuk menghitung angka dari 0 hingga tak terhingga
+
+count = 0  # Mulai dari 0
+while True:  # Loop tanpa henti
+    print(count)  # Cetak angka saat ini
+    count += 1  # Tambah angka 1 setiap iterasi
